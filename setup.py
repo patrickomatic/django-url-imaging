e8ef9963bef356d52145f6e12945b6571fff26a9
Patrick Carroll <patrick@patrickomatic.com>
2010-06-24 19:02:05 -0400

    File for use with python eggs

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+# XXX depend on boto
+setup(
+		name="django-url-imaging",
+		version="0.1",
+		description="URL-based image processing for Django",
+		author="Patrick Carroll",
+		author_email="patrick@patrickomatic.com",
+		url="http://urlimg.com/opensource",
+		packages=[])
+
