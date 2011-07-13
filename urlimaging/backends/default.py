90f4428cd0087856f4a76358786169815e27f3a7
Patrick Carroll <patick@patrickomatic.com>
2011-07-12 20:06:33 -0400

    Fixed missing format character

--- urlimaging/backends/default.py
+++ urlimaging/backends/default.py
@@ -115,15 +115,16 @@ class SCPImageStorage(ImageStorage):
 			return ""
 
 	def delete_image(self, hash):
-		os.system("ssh %(identify_file)s %(ssh_user)s \"rm %(file_path)s\"" 
-				% {'identity_file': self.identity_file_str(),
+		os.system("ssh %(identify_file)s %(ssh_user)s \"rm %(file_path)s\"" % {
+					'identity_file': self.identity_file_str(),
 					'ssh_user': settings.SSH_MEDIA_USER,
 					'file_path': os.path.join(settings.SSH_MEDIA_PATH, hash)})
 
 	def save_image(self, filename):
-		os.system("scp %(identity_file)s %(filename)s %(ssh_user)s:%(file_path)" 
-				% {'identity_file': self.identity_file_str(),
-					'ssh_user': settings.SSH_MEDIA_USER, 'filename': filename,
+		os.system("scp %(identity_file)s %(filename)s %(ssh_user)s:%(file_path)s" % {
+					'identity_file': self.identity_file_str(),
+					'ssh_user': settings.SSH_MEDIA_USER, 
+					'filename': filename,
 					'file_path': os.path.join(settings.SSH_MEDIA_PATH, hash)})
 
 	def get_image_url(self, hash):
