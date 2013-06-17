5da83d1121a720e7030e5a2d6ec965b1a1535e03
Patrick Carroll <patrick@patrickomatic.com>
2013-06-17 09:39:40 -0400

    Bumping version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 		name="django-url-imaging",
-		version="0.3.3",
+		version="0.3.4",
 		description="URL-based image processing for Django",
 		author="Patrick Carroll",
 		author_email="patrick@patrickomatic.com",
