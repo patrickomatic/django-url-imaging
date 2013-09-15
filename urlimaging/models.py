4bd9d643baf2e4da51a94c5137a6cba4f029d7cc
Kyle Derkacz <kyleder@gmail.com>
2013-09-15 14:10:51 -0700

    Fixing the url invalid issue that I previously introduced

--- urlimaging/models.py
+++ urlimaging/models.py
@@ -70,7 +70,7 @@ def sanitize_url(url):
 
 
 def valid_image_path(url):
-	return re.match(r'^(http://?)?[\w\-\.]+\.\w+(\:\d+)/.+$', url, re.I)
+	return re.match(r'^(http://?)?[\w\-\.]+\.\w+(\:\d+){0,1}/.+$', url, re.I)
 
 
 class ImageNotFoundException(Exception):
