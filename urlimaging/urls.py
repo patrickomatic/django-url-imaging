215875c023e8198ea6afcd6f5d9fd7f59b8ad7e7
Patrick Carroll <patrick@patrickomatic.com>
2014-01-13 22:27:41 -0500

    Catch an ImportError and check in the new place

--- urlimaging/urls.py
+++ urlimaging/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import patterns, urls
+except ImportError:
+    # newer versions of django have switched to this location
+    from django.conf.urls import patterns, url
+
 from urlimaging.views import modify
 
 urlpatterns = patterns('',
